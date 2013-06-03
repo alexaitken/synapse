a42a89dfa95a5c62ebbc5397753abf6d87742403
Ian Unruh <ianunruh@gmail.com>
2013-06-03 12:38:07 -0500

    MarshalSerializer now uses base64 encoding

--- lib/synapse/serialization/serializer/marshal.rb
+++ lib/synapse/serialization/serializer/marshal.rb
@@ -5,6 +5,8 @@ module Synapse
     # Note that this serializer is not necessarily the fastest serializer available, nor is it
     # flexible. Output is binary and difficult to modify, which means upcasting is not possible
     # when using this serializer.
+    #
+    # This serializer wraps the binary output of Marshal in Base64 encoding.
     class MarshalSerializer < Serializer
       # This serializer doesn't provide any configuration options
 
@@ -13,14 +15,14 @@ module Synapse
       # @param [Object] content
       # @return [Object]
       def perform_serialize(content)
-        Marshal.dump content
+        Base64.encode64(Marshal.dump(content))
       end
 
       # @param [Object] content
       # @param [Class] type
       # @return [Object]
       def perform_deserialize(content, type)
-        Marshal.load content
+        Marshal.load(Base64.decode64(content))
       end
 
       # @return [Class]
