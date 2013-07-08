711f745bf546eb811b2151480bbb41ead9a5a833
Ian Unruh <ianunruh@gmail.com>
2013-07-08 14:00:46 -0500

    Updated references to new lock manager

--- lib/synapse/repository/pessimistic_lock_manager.rb
+++ lib/synapse/repository/pessimistic_lock_manager.rb
@@ -4,25 +4,25 @@ module Synapse
     class PessimisticLockManager < LockManager
       # @return [undefined]
       def initialize
-        @aggregates = IdentifierLock.new
+        @manager = IdentifierLockManager.new
       end
 
       # @param [AggregateRoot] aggregate
       # @return [Boolean]
       def validate_lock(aggregate)
-        @aggregates.owned? aggregate.id
+        @manager.owned? aggregate.id
       end
 
       # @param [Object] aggregate_id
       # @return [undefined]
       def obtain_lock(aggregate_id)
-        @aggregates.obtain_lock aggregate_id
+        @manager.obtain_lock aggregate_id
       end
 
       # @param [Object] aggregate_id
       # @return [undefined]
       def release_lock(aggregate_id)
-        @aggregates.release_lock aggregate_id
+        @manager.release_lock aggregate_id
       end
     end # PessimisticLockManager
   end # Repository
