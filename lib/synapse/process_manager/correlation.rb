3ef98c2f6201d24dd965a6a50802f3ebcf475f1b
Ian Unruh <ianunruh@gmail.com>
2013-07-08 13:56:49 -0500

    [ProcessManager] Using struct for correlation

--- lib/synapse/process_manager/correlation.rb
+++ lib/synapse/process_manager/correlation.rb
@@ -1,32 +1,6 @@
 module Synapse
   module ProcessManager
-    # Combination key and value that is used to correlate incoming events with process instances
-    class Correlation
-      # @return [Symbol]
-      attr_reader :key
-
-      # @return [String]
-      attr_reader :value
-
-      # @param [Symbol] key
-      # @param [String] value
-      # @return [undefined]
-      def initialize(key, value)
-        @key = key.to_sym
-        @value = value.to_s
-      end
-
-      def ==(other)
-        self.class === other and
-          other.key == @key and
-          other.value == @value
-      end
-
-      alias eql? ==
-
-      def hash
-        @key.hash ^ @value.hash
-      end
-    end # Correlation
-  end # ProcessManager
+    # Combination key and value that is used to correlate incoming events with process instance
+    Correlation = Struct.new :key, :value
+  end
 end
