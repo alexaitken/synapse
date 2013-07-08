fd40e8f7e4bdde421644ab2ed340f02ffeace69c
Ian Unruh <ianunruh@gmail.com>
2013-07-08 14:00:46 -0500

    Updated references to new lock manager

--- lib/synapse/process_manager/pessimistic_lock_manager.rb
+++ lib/synapse/process_manager/pessimistic_lock_manager.rb
@@ -3,20 +3,20 @@ module Synapse
     # Lock manager that blocks until a lock can be obtained for a process
     class PessimisticLockManager
       def initialize
-        @lock = IdentifierLock.new
+        @manager = IdentifierLockManager.new
       end
 
       # @param [String] process_id
       # @return [undefined]
       def obtain_lock(process_id)
-        @lock.obtain_lock process_id
+        @manager.obtain_lock process_id
       end
 
       # @raise [ThreadError] If thread didn't previously hold the lock
       # @param [String] process_id
       # @return [undefined]
       def release_lock(process_id)
-        @lock.release_lock process_id
+        @manager.release_lock process_id
       end
     end # PessimisticLockManager
   end # ProcessManager
