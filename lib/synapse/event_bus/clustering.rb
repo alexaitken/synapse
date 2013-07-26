ff32fb2e315b41b5682d1fcc0ef3acd5ffbc146f
Ian Unruh <ianunruh@gmail.com>
2013-07-26 10:52:32 -0500

    [EventBus] Added cluster selector using class name patterns

--- lib/synapse/event_bus/clustering.rb
+++ lib/synapse/event_bus/clustering.rb
@@ -3,6 +3,7 @@ require 'synapse/event_bus/clustering/base_cluster'
 require 'synapse/event_bus/clustering/simple_cluster'
 
 require 'synapse/event_bus/clustering/selector'
+require 'synapse/event_bus/clustering/class_pattern_selector'
 require 'synapse/event_bus/clustering/composite_selector'
 require 'synapse/event_bus/clustering/simple_selector'
 
---
+++ spec/event_bus/clustering/class_pattern_selector_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module Synapse
+  module EventBus
+
+    describe ClassPatternClusterSelector do
+      it 'selects a cluster when the class name matches a pattern' do
+        Foo = Class.new
+        Bar = Class.new
+
+        cluster = Object.new
+        pattern = /Foo/
+
+        selector = ClassPatternClusterSelector.new cluster, pattern
+
+        selector.select_for(Foo.new).should be(cluster)
+        selector.select_for(Bar.new).should be_nil
+      end
+    end
+
+  end
+end
